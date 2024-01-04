88b94a2dd34e7bc4f5ae35db48498959e6801a59
megaconfidence <cokoghenun@gmail.com>
2024-01-04 09:34:07 +0100

    feat: Adds ui

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
+const appUI = document.querySelector(".ui");
+const bpmTxt = document.querySelector(".bpm");
+const stopBTN = document.querySelector(".stop");
+const heartUI = document.querySelector(".heart");
+const startBTN = document.querySelector(".start");
+const errorTxt = document.querySelector(".error");
+const beatAudio = document.querySelector("audio");
+const connectBTN = document.querySelector(".connect");
+
 let device;
 let heartRate;
-const ui = document.querySelector("#ui");
+
+function parseHeartRate(value) {
+  let is16Bits = value.getUint8(0) & 0x1;
+  if (is16Bits) return value.getUint16(1, true);
+  return value.getUint8(1);
+}
 
 function handleRateChange(event) {
-  console.log(event.target.value);
-  ui.textContent = parseHeartRate(event.target.value);
+  bpmTxt.textContent = parseHeartRate(event.target.value);
 }
 
 async function requestDevice() {
-  const _options = {
-    acceptAllDevices: true,
-    optionalServices: ["battery_service"],
-  };
-  const options = { filters: [{ services: ["heart_rate"] }] };
+  //only works for devices advertising heart rate service
+  const _options = { filters: [{ services: ["heart_rate"] }] };
 
+  const options = {
+    acceptAllDevices: true,
+    optionalServices: ["heart_rate"],
+  };
   device = await navigator.bluetooth.requestDevice(options);
   device.addEventListener("gattserverdisconnected", connectDevice);
 }
 
 async function connectDevice() {
-  //takes a few seconds to connect
   if (device.gatt.connected) return;
 
   const server = await device.gatt.connect();
@@ -30,19 +43,30 @@ async function connectDevice() {
   console.log("connected");
 }
 
-document.querySelector("#connect").addEventListener("click", async () => {
-  if (!device) await requestDevice();
-  connectDevice();
-});
-document.querySelector("#start").addEventListener("click", async () => {
+async function startMonitoring() {
+  beatAudio.play();
   await heartRate.startNotifications();
-});
-document.querySelector("#stop").addEventListener("click", async () => {
-  await heartRate.stopNotifications();
-});
-
-function parseHeartRate(value) {
-  let is16Bits = value.getUint8(0) & 0x1;
-  if (is16Bits) return value.getUint16(1, true);
-  return value.getUint8(1);
+  heartUI.classList.remove("pause-animation");
 }
+
+async function stopMonitoring() {
+  beatAudio.pause();
+  await heartRate.stopNotifications();
+  heartUI.classList.add("pause-animation");
+}
+
+async function init() {
+  if (!navigator.bluetooth) return errorTxt.classList.remove("hide");
+  if (!device) await requestDevice();
+
+  connectBTN.textContent = "connecting...";
+  await connectDevice();
+
+  appUI.classList.remove("hide");
+  connectBTN.classList.add("hide");
+  await startMonitoring();
+}
+
+connectBTN.addEventListener("click", init);
+stopBTN.addEventListener("click", stopMonitoring);
+startBTN.addEventListener("click", startMonitoring);
